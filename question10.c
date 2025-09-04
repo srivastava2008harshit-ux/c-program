c8b7a599bec03f029ba87ddad6e66757492064f1
srivastava2008harshit-ux <srivastava2008harshit@gmail.com>
2025-09-04 13:49:23 +0530

    Add files via upload

---
+++ question10.c
@@ -0,0 +1,17 @@
+// write a program to find the square root of a number.
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   float a , sq;
+   printf("enter a number:");
+   scanf("%f", &a);
+   
+   sq= sqrt(a);
+   printf("the square root of %f = %f\n", a , sq);
+   return 0;
+   }
+   
+   
